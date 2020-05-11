392af116fb71f68868e5106304d8040baeae6195
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-11 14:03:19 -0300

    Criado tipos primitivos, wrappers, não primitivos e tipagem forte e estatica arquivos .java .

---
+++ src/main/java/primitivos/DefaultValues/naoprimitivos.java
@@ -0,0 +1,16 @@
+package primitivos.DefaultValues;
+
+public class naoprimitivos {
+    public static void main(String[] args){
+
+        String texto = "Meu texto para apresentação " ; //Sequência de caracteres.
+
+        Void v; //Tipo válido.
+
+        Object o = new Object();
+
+        Number numero = 100;
+
+        numero.toString ();
+    }
+}
---
+++ src/main/java/tipagem/tipageminferido.java
@@ -0,0 +1,15 @@
+package tipagem;
+
+public class tipageminferido {
+
+    public static void main(String[] args){
+
+        var numero = Integer.valueOf("13456");
+
+        var texto = "O numero é :";
+
+        System.out.println (texto + numero);
+
+    }
+
+}
