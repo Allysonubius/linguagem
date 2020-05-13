aa64c6b30fb774cda9fa3a33f3a3b31affd2218f
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-13 15:05:49 -0300

    Criado wrappers unboxing onde há a criação inicial e anexado os não primitivos dos comandos Java.

---
+++ Linguagem/src/main/java/TIPOS/primitivos/naoprimitivos.java
@@ -0,0 +1,19 @@
+package TIPOS.primitivos;
+
+public class naoprimitivos {
+
+    public static void main(String[]args){
+
+        String texto = "Meu texto para apresentação";   //Sequência de caracteres
+
+        Void v; //Tipo válido
+
+        Object o = new Object();
+
+        Number numero = Integer.valueOf (100);
+
+        numero.toString ();
+
+    }
+
+}
---
+++ Linguagem/src/main/java/TIPOS/primitivos/wrappersunboxing.java
@@ -0,0 +1,18 @@
+package TIPOS.primitivos;
+
+public class wrappersunboxing {
+
+    public static void main(String[] args){
+
+        //Unboxing
+
+        int i = new Integer(3);
+
+        int inteiro = Integer.valueOf (1024);
+
+        //boolean b = new Boolean(true);
+        boolean b2 = Boolean.TRUE;
+        boolean b3 = Boolean.getBoolean ("false");
+    }
+
+}
