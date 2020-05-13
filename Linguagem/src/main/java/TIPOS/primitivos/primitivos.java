9b12585605183cf724ad0762aabe8c753166e16b
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-13 13:51:11 -0300

    Finalizado edição do programa SuperUse.Iniciado entendimento sobre tipos.

---
+++ Linguagem/src/main/java/TIPOS/primitivos/primitivos.java
@@ -0,0 +1,18 @@
+package TIPOS.primitivos;
+
+public class primitivos {
+
+    public static void main(String[] args){
+
+    //INTEIROS
+
+        //byte nullByte = null;
+        //Bytes não podem ser primitivos.
+
+        byte b;
+        byte b1 = 127;
+        byte b2 = -128;
+
+
+    }
+}
--- Linguagem/src/main/java/linguagem/Allysonubius/Linguagem/classes/usuario/programa.java
+++ Linguagem/src/main/java/linguagem/Allysonubius/Linguagem/classes/usuario/programa.java
@@ -1,7 +1,7 @@
 package linguagem.Allysonubius.Linguagem.classes.usuario;
 
 public class programa {
-    public static void main (String[] args)
-    {System.out.println ("Hello Wordl!");
+    public static void main (String[] args) {
+        System.out.println ("Hello Wordl!");
     }
 }
