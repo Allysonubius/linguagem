f7d02e968657d02c575ea0b636f926c059445ab4
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-13 13:33:19 -0300

    Criado comando  hello horld.

---
+++ Linguagem/src/main/java/linguagem/Allysonubius/Linguagem/classes/usuario/programa.java
@@ -0,0 +1,7 @@
+package linguagem.Allysonubius.Linguagem.classes.usuario;
+
+public class programa {
+    public static void main (String[] args)
+    {System.out.println ("Hello Wordl!");
+    }
+}
