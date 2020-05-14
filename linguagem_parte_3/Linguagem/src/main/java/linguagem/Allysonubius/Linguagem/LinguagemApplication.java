892d6951982d80c14fea439ec5e99d9bf54eb1bc
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-14 14:27:33 -0300

    Iniciando linguagem_3 - Aplicando teste ou Debugs .

---
+++ linguagem_parte_3/Linguagem/src/main/java/linguagem/Allysonubius/Linguagem/LinguagemApplication.java
@@ -0,0 +1,13 @@
+package linguagem.Allysonubius.Linguagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguagemApplication.class, args);
+	}
+
+}
---
+++ linguagem_parte_3/Linguagem/src/test/java/linguagem/Allysonubius/Linguagem/LinguagemApplicationTests.java
@@ -0,0 +1,13 @@
+package linguagem.Allysonubius.Linguagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
